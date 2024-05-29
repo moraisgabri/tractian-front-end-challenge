cb8babf423dcf789d41a7a56b930cfc59ff33c05
Gabriel Morais <gabriel8azevedo@gmail.com>
2024-05-29 05:19:49 -0300

    refactor(TreeView): remove logs

--- src/components/TreeView/index.tsx
+++ src/components/TreeView/index.tsx
@@ -1,15 +1,12 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC } from "react";
 import styles from "./styles.module.css";
 import { useScreenContentContext } from "@/contexts/ScreenContentContext.ts";
-import { Button } from "../Button";
 import { TreeNode } from "../TreeViewNode";
 import { TreeViewNode } from "@/types/components/treeView";
 import { TreeViewSearchInput } from "../TreeViewSearchInput";
 
 export const TreeView: FC = () => {
   const { nodes } = useScreenContentContext();
-
-  console.log({ nodes });
 
   return (
     <div className={styles.treeViewWrapper}>
